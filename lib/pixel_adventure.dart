84fac08861ba48e73fdf170d4a242fa21ddb1987
AlexForget <alexandreqc26@gmail.com>
2024-01-09 14:22:12 -0500

    add assets, creates the world and camera

---
+++ lib/pixel_adventure.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:platfom_game/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xff211f30);
+  late final CameraComponent cam;
+  @override
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 640, height: 360);
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+    return super.onLoad();
+  }
+}
